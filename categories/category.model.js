c2160e747ec859406afa5d63d26b7bbc5728cbcd
Jhonatan Silva da Costa <jhonatan.costa@tuntscorp.com>
2022-11-06 18:35:16 -0300

    feat(blog-project): Model de categoria

---
+++ categories/category.model.js
@@ -0,0 +1,14 @@
+//model variables
+const sequelize = require('sequelize');
+const connection = require('../database/database');
+
+
+//create a categories model ante define categories table
+const category_model = connection.define('categories', {
+    title: { type: sequelize.STRING, allowNull: false },
+    slug: { type: sequelize.STRING, allowNull: false }
+});
+
+
+//exports
+module.exports = category_model;
