9a8f3fe6b17c94fff766a3bf2b65c2b66e77e640
Jhonatan Silva da Costa <jhonatan.costa@tuntscorp.com>
2022-11-22 09:39:29 -0300

    feat(blog-project): Formulário de categoria

--- categories/categories.controller.js
+++ categories/categories.controller.js
@@ -2,12 +2,9 @@
 const express = require('express');
 const router = express.Router();
 
-
-//Categories route
-router.get('/categories', (req, res) => {
-    res.send('Categories route');
+router.get('/admin/categories/new', (req, res) => {
+    res.render('admin/categories/new');
 });
-
 
 //exports
 module.exports = router;
